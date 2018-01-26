1642ab89d9033a946b85ac6056fa33725a49c83a
Jan-Cord <gnaarf@gmx.de>
2018-01-26 22:10:13 +0100

    dummy WaveManager

---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public float EvaluateWaveHeight(Vector2 position)
+    {
+        return 0;
+    }
+
+    public Vector2 EvaluateWaveGradient(Vector2 position)
+    {
+        return Vector2.zero;
+    }
+}
