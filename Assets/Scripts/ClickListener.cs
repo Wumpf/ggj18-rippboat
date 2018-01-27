a10156a13442d2c53e9d86968e4dae27eeed9e9d
Gerd Schmidt <gerd-has@awesome-co.de>
2018-01-26 23:11:19 -0500

    Added 3D Gamepad Input

--- Assets/Scripts/ClickListener.cs
+++ Assets/Scripts/ClickListener.cs
@@ -9,6 +9,8 @@ public class ClickListener : MonoBehaviour
 
     Collider WaveCollider;
 
+    public GamepadCursor[] Cursors;
+
 
     // Use this for initialization
     void Start()
@@ -26,6 +28,14 @@ public class ClickListener : MonoBehaviour
         if(Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hitInfo) && hitInfo.collider == WaveCollider)
         {
             WaveManager.AddWave(hitInfo.point, Time.time);
+        }
+
+        foreach (var cursor in Cursors)
+        {
+            if (cursor.MakeWave)
+            {
+                WaveManager.AddWave(cursor.CursorOnSurface.position, Time.time);
+            }
         }
     }
 
