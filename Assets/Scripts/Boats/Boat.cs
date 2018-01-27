ba203217e0b29a94edddcaae40a855185de87490
Andreas Reich <r_andreas2@web.de>
2018-01-27 16:17:37 +0100

    fixed zero rotation errors in boat

--- Assets/Scripts/Boats/Boat.cs
+++ Assets/Scripts/Boats/Boat.cs
@@ -21,13 +21,15 @@ public class Boat : MonoBehaviour
         Vector3 gradient = WaveManager.EvaluateWaveGradient(transform.position, out normal);
 
 		// more magnitude, more rotating! This also protects against weirdness when nothing is going on! :)
-		float rotationFactor = gradient.sqrMagnitude; 
-		
-		// rotate towards gradient. hackidihack!!!
-		Quaternion current = transform.rotation;
-		Quaternion target = Quaternion.LookRotation(-gradient.normalized, normal) * Quaternion.FromToRotation(Vector3.right, -gradient.normalized);
-		//http://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
-		transform.rotation = Quaternion.Slerp(current, target, 1.0f - Mathf.Exp(-Time.deltaTime * 6f * rotationFactor));
+		float rotationFactor = gradient.sqrMagnitude * 6.0f;
+		if (rotationFactor > 0.000001f) // preventing errros and stuff
+		{
+			// rotate towards gradient. hackidihack!!!
+			Quaternion current = transform.rotation;
+			Quaternion target = Quaternion.LookRotation(-gradient.normalized, normal) * Quaternion.FromToRotation(Vector3.right, -gradient.normalized);
+			//http://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
+			transform.rotation = Quaternion.Slerp(current, target, 1.0f - Mathf.Exp(-Time.deltaTime * rotationFactor));
+		}
 	}
 
 	
