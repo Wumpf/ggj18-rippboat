279f159caab1e430c4175005ac4826a151fe2153
Jan-Cord <gnaarf@gmx.de>
2018-01-27 01:19:39 +0100

    'calculated' Gradient ... probably... maybe.... (?)

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -12,7 +12,7 @@ struct Wave
         OriginPosition = new Vector3(originPosition.x, 0F, originPosition.z);
         StartTime = startTime;
     }
-
+    
     public float EvaluateWaveHeight(Vector3 position, float time)
     {
         position = new Vector3(position.x, 0F, position.z);
@@ -22,12 +22,20 @@ struct Wave
         if (d > t * 2F)
             return 0;
 
-        return t > 0 ? Mathf.Sin(t * 2F - d) / (t * 3F + 4F) * 6F : 0F;
-
+        return Mathf.Sin(t * 2F - d) / (t * 3F + 4F) * 6F; // fabolous function
     }
 
     public Vector3 EvaluateWaveGradient(Vector3 position, float time)
     {
-        return Vector3.zero;
+        position = new Vector3(position.x, 0F, position.z);
+        float d = Vector3.Distance(OriginPosition, position);
+        float t = time - StartTime;
+
+        if (d > t * 2F)
+            return Vector3.zero;
+
+        Vector3 direction = position - OriginPosition;
+        direction = new Vector3(direction.x, 0F, direction.z).normalized;
+        return new Vector3(direction.x, -Mathf.Cos(t * 2F - d) / (t * 3F + 4F) * 6F, direction.z); // fabolous function #2
     }
 }
