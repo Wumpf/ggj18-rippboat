08c28783f63ca1a542ff33c3c2df042b61177b7e
Gerd Schmidt <gerd-has@awesome-co.de>
2018-01-27 13:24:58 -0500

    Added health bar and boat wall bounce! #BouncyBounce

--- Assets/Scripts/Boats/Boat.cs
+++ Assets/Scripts/Boats/Boat.cs
@@ -3,42 +3,67 @@ using System.Collections.Generic;
 using Assets.Scripts;
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 public class Boat : MonoBehaviour
 {
     private WaveManager WaveManager;
-	public Player Owner = Player.ONE;
+    public Player Owner = Player.ONE;
+    private Health _health;
+
+    private Vector3 _bounceForce;
+
+    public float BounceMagnitude = 0.9f;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         WaveManager = FindObjectOfType<WaveManager>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{//transform.position = 0.
+        _health = GetComponentInChildren<Health>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = 0.
 
         Vector3 normal;
         Vector3 gradient = WaveManager.EvaluateWaveGradient(transform.position, out normal);
 
-		// more magnitude, more rotating! This also protects against weirdness when nothing is going on! :)
-		float rotationFactor = gradient.sqrMagnitude * 6.0f;
-		if (rotationFactor > 0.000001f) // preventing errros and stuff
-		{
-			// rotate towards gradient. hackidihack!!!
-			Quaternion current = transform.rotation;
-			Quaternion target = Quaternion.LookRotation(-gradient.normalized, normal) * Quaternion.FromToRotation(Vector3.right, -gradient.normalized);
-			//http://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
-			transform.rotation = Quaternion.Slerp(current, target, 1.0f - Mathf.Exp(-Time.deltaTime * rotationFactor));
-		}
-	}
+        // more magnitude, more rotating! This also protects against weirdness when nothing is going on! :)
+        float rotationFactor = gradient.sqrMagnitude * 6.0f;
+        if (rotationFactor > 0.000001f) // preventing errros and stuff
+        {
+            // rotate towards gradient. hackidihack!!!
+            Quaternion current = transform.rotation;
+            Quaternion target = Quaternion.LookRotation(-gradient.normalized, normal) * Quaternion.FromToRotation(Vector3.right, -gradient.normalized);
+            //http://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
+            transform.rotation = Quaternion.Slerp(current, target, 1.0f - Mathf.Exp(-Time.deltaTime * rotationFactor));
+        }
+    }
 
-	
-	void OnCollisionEnter(Collision collision)
-	{
-		if (collision.transform.tag == "Reef")
-		{
-			GameObject.Destroy(this.gameObject);
-		}
+    void FixedUpdate()
+    {
+        this.transform.position += _bounceForce;
+        _bounceForce *= 0.9f;
+    }
 
-	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Reef")
+        {
+            _health.AddDamage(1);
+
+            if (_health.HP <= 0)
+                GameObject.Destroy(this.gameObject);
+
+            var gradient = WaveManager.EvaluateWaveGradient(transform.position);
+            float bounceFactor = Mathf.Max(0.1f, gradient.magnitude);
+
+            _bounceForce += bounceFactor * collision.contacts[0].normal * BounceMagnitude;
+        }
+
+    }
+
+
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public float HP = 10;
+    public float MaxHP = 10;
+
+    public float Speed = 1;
+
+    public Transform PointBar;
+    
+	// Use this for initialization
+	void Start ()
+	{
+	    RecalculateBar();
+
+	}
+
+    public void AddDamage(float ammount)
+    {
+        HP -= ammount;
+        if(HP>0)
+            RecalculateBar();
+    }
+
+    private void RecalculateBar()
+    {
+        var percentage = (HP / MaxHP);
+        PointBar.transform.localScale = new Vector3(percentage * 0.95f, PointBar.transform.localScale.y, PointBar.transform.localScale.z);
+    }
+
+    void FixedUpdate()
+    {
+        var initialRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z);
+
+        transform.LookAt(Camera.main.transform);
+        var targetRoation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z);
+
+        transform.localRotation = Quaternion.Lerp(initialRotation, targetRoation, 0.1f);
+    }
+}
