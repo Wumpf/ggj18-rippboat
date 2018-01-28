b2622fe082e6cc8f3f96c5a8ee12f22597623209
Jan-Cord <gnaarf@gmx.de>
2018-01-28 19:20:07 +0100

    WinnerIcon FadeIn

--- Assets/Scripts/WinnerIconDisplayer.cs
+++ Assets/Scripts/WinnerIconDisplayer.cs
@@ -36,16 +36,37 @@ public class WinnerIconDisplayer : MonoBehaviour {
                 renderer.sprite = DefaultIcon;
                 break;
         }
+
+        float fadeInDuration = 2F;
+        StartCoroutine(FadeIn(fadeInDuration));
     }
 
     public void ResetWinnerIcon()
     {
         renderer.sprite = DefaultIcon;
+
+        float fadeInDuration = 2F;
+        StartCoroutine(FadeIn(fadeInDuration));
     }
 
         // Use this for initialization
     void Start () {
         renderer = GetComponent<SpriteRenderer>();
         renderer.sprite = DefaultIcon;
+    }
+
+    IEnumerator FadeIn(float time)
+    {
+        float countdown = time;
+        Color c = renderer.material.color;
+
+        while (time > 0F)
+        {
+            renderer.material.color = new Color(c.r, c.g, c.b, 1F - countdown / time);
+            countdown -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        renderer.material.color = new Color(c.r, c.g, c.b, 1F);
     }
 }
